f1f72a22c74286af5a81b09043d7823565efe4d6
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2022-11-26 14:08:42 +0100

    daq_not_on_tape_runs

--- cygno/__init__.py
+++ cygno/__init__.py
@@ -599,8 +599,8 @@ def daq_read_runlog_replica_status(connection, run_number, storage, verbose=Fals
 
 def daq_not_on_tape_runs(connection, verbose=False):
     import numpy as np
-    sql = "SELECT * FROM `Runlog` WHERE DATEDIFF(`start_time`, CURRENT_TIMESTAMP) \
-    BETWEEN 0 AND 30 AND `storage_tape_status` < 1 AND `storage_cloud_status` = 1;"
+    sql = "SELECT * FROM `Runlog` WHERE DATEDIFF(CURRENT_TIMESTAMP, start_time) < 30 \
+    AND `storage_tape_status` < 1 AND `storage_cloud_status` = 1;"
     mycursor = connection.cursor()
     mycursor.execute(sql)
     value = mycursor.fetchall()

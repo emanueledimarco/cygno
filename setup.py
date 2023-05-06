e26f664c0e57643a053c6f5ed6095e85a473a20d
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2023-05-06 07:40:57 +0200

    DAQ/Waveform Correction v1.0.12.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='cygno',                                            # package name
     author="Giovanni Mazzitelli",
     author_email="giovanni.mazzitelli@lnf.infn.it",
-    version='1.0.11',                                         # version
+    version='1.0.12',                                         # version
     description='Cygno Experiment Python Packge',            # short description
     url='https://github.com/CYGNUS-RD/cygno',                # package URL
     packages=find_packages(),
